fef588ea98f359225b62ebf0aef058a3a2858baa
aintira11 <aintrlala@gmail.com>
2024-12-28 22:49:07 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet
+            ,RouterModule
+            ,HomeComponent
+            ,HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: HomeComponent},
+    {path: 'login',component: LoginComponent},
+    {path: 'login',component: RegisterComponent},
+    // { 
+    //   path: 'myprofile',
+    //   component: MyprofileComponent,
+    //   children: [
+    //       { path: 'post', component: MyprofilePostComponent },
+       
+    //   //   { path: 'test/:id', component: TestComponent },
+    //   ],
+    // },
+  ];
