56cebb51a5e66ec0be3218c860d3d2203cbe60cd
aintira11 <aintrlala@gmail.com>
2025-01-12 17:17:39 +0700

    create

--- src/app/components/user/create-useracc/create-useracc.component.ts
+++ src/app/components/user/create-useracc/create-useracc.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-useracc',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatButtonModule,
+     RouterModule
+    ],
   templateUrl: './create-useracc.component.html',
   styleUrl: './create-useracc.component.scss'
 })
 export class CreateUseraccComponent {
-
+  constructor(private router : Router,){}
+  
+    register() {
+      this.router.navigate(['/creatfro']);
+      }
 }
