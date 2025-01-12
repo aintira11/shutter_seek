1b3163eacf8a39111dbd0bb8eae9d25c417b6249
pitchazerothree <161620273+pitchazerothree@users.noreply.github.com>
2025-01-12 17:03:51 +0700

    in

--- src/app/components/user/edit-package/edit-package.component.ts
+++ src/app/components/user/edit-package/edit-package.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './edit-package.component.scss'
 })
 export class EditPackageComponent {
-
+     // Array สำหรับเก็บข้อมูลแพ็กเกจ
+     packages = [
+      { category: 'สินค้า', price: '5,999', details: '' }
+    ];
+  
+    // ฟังก์ชันสำหรับเพิ่มแพ็กเกจใหม่
+    addPackage() {
+      this.packages.push({ category: 'สินค้า', price: '', details: '' });
+    }
+  
+    // ฟังก์ชันสำหรับลบแพ็กเกจ
+    deletePackage(index: number) {
+      this.packages.splice(index, 1);
+    }
 }
