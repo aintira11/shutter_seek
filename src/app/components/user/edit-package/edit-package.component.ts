9c0de77a757b514b1c670b790e386f9fbb7c73a3
pitchazerothree <161620273+pitchazerothree@users.noreply.github.com>
2025-01-12 16:51:30 +0700

    update

---
+++ src/app/components/user/edit-package/edit-package.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-package',
+  imports: [],
+  templateUrl: './edit-package.component.html',
+  styleUrl: './edit-package.component.scss'
+})
+export class EditPackageComponent {
+
+}
