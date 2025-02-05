733ca75b1f1a90dedf97c082399bfb708e1a82ef
aintira11 <aintrlala@gmail.com>
2025-02-06 03:12:58 +0700

    aaaa

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -24,15 +24,17 @@ export class HomeComponent implements OnInit{
   datauser: DataMembers[] = [];
   Tags:DataTegs[]=[];
   Portfolio:DataPortfolio[]=[];
+  PortfolioID:DataPortfolio[]=[];
 
   // กำหนด index ของแต่ละ Portfolio ที่แสดง
   currentSlideIndex: number[] = [];
+  currentSlideIndex1: number[] = [];
 
   constructor(private Constants: Constants, private route: ActivatedRoute, private http: HttpClient,private router : Router){
 
     // กำหนดให้ทุก Portfolio เริ่มต้น index ของการแสดงที่ 0
-    this.Portfolio.forEach(() => this.currentSlideIndex.push(0));
-    
+    // this.Portfolio.forEach(() => this.currentSlideIndex.push(0));
+    // this.PortfolioID.forEach(() => this.currentSlideIndex1.push(0));
   }
 
   ngOnInit(): void {
@@ -49,27 +51,46 @@ export class HomeComponent implements OnInit{
         this.gettegs();
         this.getPortfolio();
       });
+      this.Portfolio.forEach((_, index) => {
+        this.currentSlideIndex[index] = 0;
+      });
+      this.PortfolioID.forEach((_, index) => {
+        this.currentSlideIndex1[index] = 0;
+      });      
   } 
   // ฟังก์ชันสำหรับการแสดงภาพปัจจุบัน
-  getSlide(index: number, portfolioIndex: number) {
-    const image = this.Portfolio[portfolioIndex]?.image_urls?.[index];
-    console.log("Slide Image URL:", image);
-    return image;
- }
+//   getSlide(index: number, portfolioIndex: number) {
+//     const image = this.Portfolio[portfolioIndex]?.image_urls?.[index];
+//     console.log("Slide Image URL:", image);
+//     return image;
+//  }
  
  // ฟังก์ชันเพื่อไปยังภาพถัดไป (เลื่อนเฉพาะ portfolio ของตัวเอง)
 getNext(portfolioIndex: number) {
-  if (this.Portfolio[portfolioIndex] && this.Portfolio[portfolioIndex].image_urls) {
+  // if (this.Portfolio[portfolioIndex] && this.Portfolio[portfolioIndex].image_urls) {
+    if (this.Portfolio[portfolioIndex]?.image_urls?.length > 0) {
     const maxIndex = this.Portfolio[portfolioIndex].image_urls.length - 1;
     this.currentSlideIndex[portfolioIndex] = (this.currentSlideIndex[portfolioIndex] + 1) % (maxIndex + 1);
   }
+  if (this.PortfolioID[portfolioIndex]?.image_urls?.length > 0) {
+    const maxIndex = this.PortfolioID[portfolioIndex].image_urls.length - 1;
+    this.currentSlideIndex1[portfolioIndex] = (this.currentSlideIndex1[portfolioIndex] + 1) % (maxIndex + 1);
+    console.log(this.currentSlideIndex1);
+  }
+
 }
 
 // ฟังก์ชันเพื่อไปยังภาพก่อนหน้า (เลื่อนเฉพาะ portfolio ของตัวเอง)
 getPrev(portfolioIndex: number) {
-  if (this.Portfolio[portfolioIndex] && this.Portfolio[portfolioIndex].image_urls) {
+  // if (this.Portfolio[portfolioIndex] && this.Portfolio[portfolioIndex].image_urls) {
+    if (this.Portfolio[portfolioIndex]?.image_urls?.length > 0) {
     const maxIndex = this.Portfolio[portfolioIndex].image_urls.length - 1;
     this.currentSlideIndex[portfolioIndex] = (this.currentSlideIndex[portfolioIndex] - 1 + (maxIndex + 1)) % (maxIndex + 1);
+  }
+  if (this.PortfolioID[portfolioIndex]?.image_urls?.length > 0) {
+    const maxIndex = this.PortfolioID[portfolioIndex].image_urls.length - 1;
+    this.currentSlideIndex1[portfolioIndex] = (this.currentSlideIndex1[portfolioIndex] - 1 + (maxIndex + 1)) % (maxIndex + 1);
+    console.log(this.currentSlideIndex1);
   }
 }
 
@@ -104,7 +125,7 @@ getPrev(portfolioIndex: number) {
         }
     }
     getPortfolio(){
-      const url = this.Constants.API_ENDPOINT + '/shutter/get/portfolio' ;
+      const url = this.Constants.API_ENDPOINT + '/home/get/portfolio' ;
       this.http.get(url).subscribe((response: any) => {
         this.Portfolio = response;
         this.currentSlideIndex = new Array(this.Portfolio.length).fill(0); // กำหนด index ให้ทุก portfolio
@@ -112,5 +133,18 @@ getPrev(portfolioIndex: number) {
       });
     }
 
+    getPortfolioIfId(id: number) {
+      console.log('id:', id);
+      const url = this.Constants.API_ENDPOINT + '/home/get/portfolio/' + id;
+      this.http.get(url).subscribe((response: any) => {
+        this.PortfolioID = response;
+    
+        // รีเซ็ตค่า index ของสไลด์ให้ตรงกับจำนวนของ Portfolio ที่เปลี่ยนไป
+        this.currentSlideIndex1 = new Array(this.PortfolioID.length).fill(0);
+    
+        console.log("data PortfolioID:", this.PortfolioID);
+      });
+    }
+    
     
 }
